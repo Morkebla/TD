8aa02f0636cb3fbf9943223e86ee84248c27ab25
Morkebla <109845755+Morkebla@users.noreply.github.com>
2022-10-24 19:06:30 +0300

    Targeting
    Targeting the closest enemy scripts added.

--- Assets/Prefabs/Tower/TargetLocator.cs
+++ Assets/Prefabs/Tower/TargetLocator.cs
@@ -1,24 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
-    [SerializeField] Transform weapon; // used to set our weapon a.k.a tower prefab.
+    [SerializeField] Transform weapon; // used to set our weapon a.k.a tower prefab.7
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
 
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform; // here we find our target in the world.
-    }
 
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position , enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
+    }
+
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance (transform.position , target.transform.position);
+
         weapon.LookAt(target); // here we tell our weapon to look at the found Target.
-        
+        if(targetDistance < range)
+        {
+            Attack(true);
+        } 
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
